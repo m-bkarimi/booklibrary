82e02f727cc53aa802ffdb16ef9606f1efc0401f
bacs <bacspython@gmail.com>
2021-04-21 14:58:27 -0400

    Added user endpoint

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third party app
+    'rest_framework',
+    'rest_framework.authtoken',
     # local app
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
